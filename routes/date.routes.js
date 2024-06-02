097fa32fd81ffc81747a88b75125af84c0178e82
Alfonso <alfonsomanuel2000@gmail.com>
2024-06-02 14:08:03 +0200

    in progress 2

--- routes/date.routes.js
+++ routes/date.routes.js
@@ -1,5 +1,55 @@
-const router = require("express").Router()
+const router = require("express").Router();
+const Date = require("../models/Date_model");
 
+//DATE
 
+//----
+router.post("/", (req, res, next) => {
+  Date.create({
+    day: req.body.day,
+    hour: req.body.hour,
+  })
+    .then(() => {
+      console.log("cita creada");
+      res.status(201).json;
+    })
+    .catch((error) => {
+      console.log(error);
+      next(error);
+    });
+});
 
-module.exports = router
+//-----
+router.get("/", async (req, res, next) => {
+  try {
+    const response = await Date.find();
+    res.status(200).json(response);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//----
+router.get("/:id", async (req, res, next) => {
+  try {
+    const response = await Date.find();
+    res.status(200).json(response);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const response = await Date.findByIdAndUpdate(req.params.id, {
+      day: req.body.day,
+      hour: req.body.hour,
+    });
+    res.status(200).json(response);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+module.exports = router;
